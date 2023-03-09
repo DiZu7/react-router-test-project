b10635d9add8214421d6f7bc164b5ddb40981a9c
DiZu7 <dianadovhanyuk@gmail.com>
2023-03-09 11:57:31 +0200

    updated

--- src/gateway/gateway.js
+++ src/gateway/gateway.js
@@ -1,7 +1,7 @@
 const baseUrl = 'https://api.github.com/users';
 
-export const fetchData = (id) => {
-  return fetch(`${baseUrl}/${id}`).then((response) => {
+export const fetchData = (userId) => {
+  return fetch(`${baseUrl}/${userId}`).then((response) => {
     if (!response.ok) {
       throw new Error('failed request');
     }
